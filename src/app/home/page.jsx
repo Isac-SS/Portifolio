f0b6879bcd50f139e7e93927e2919c00ddaf657a
Isac Santos de Sá <isacsants679@gmail.com>
2024-04-09 16:23:11 -0300

    feat: add initial home

---
+++ src/app/home/page.jsx
@@ -0,0 +1,64 @@
+// eslint-disable-next-line no-unused-vars
+import TitleHome from "@/components/title";
+import { Button } from "@/components/ui/button";
+import { Linkedin } from "lucide-react";
+import { Github } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { ExternalLink } from "lucide-react";
+
+import React from "react";
+
+function HomeMain() {
+  return (
+    <Card
+      className="bg-slate-100 flex flex-col justify-center p-16"
+      style={{ height: "calc(100vh - 96px)" }}
+    >
+      <TitleHome />
+      <div className="flex mt-8 gap-4">
+        <Button
+          variant="ghost"
+          size="icon"
+          title="Currículo"
+          className="hover:bg-[#088C37] "
+        >
+          <ExternalLink />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          title="Linkedin"
+          className="hover:bg-[#088C37]"
+        >
+          <a
+            href="https://www.linkedin.com/feed/"
+            title="Linkedin"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:bg-[#088C37]"
+          >
+            <Linkedin />
+          </a>
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          title="GitHub"
+          className="hover:bg-[#088C37]"
+        >
+          <a
+            href="https://github.com/Isac-SS/"
+            title="Linkedin"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:bg-[#088C37]"
+          >
+            <Github />
+          </a>
+        </Button>
+      </div>
+    </Card>
+  );
+}
+
+export default HomeMain;
---
+++ src/components/title.jsx
@@ -0,0 +1,11 @@
+
+const TitleHome = () => {
+  return (
+    <div className="flex justify-center flex-col gap-2">
+      <p  className="font-semibold text-[#088C37] text-4xl md:text-6xl lg:text-7xl xl:text-8xl">Olá, sou o Isac Santos,</p>
+      <h1 className="font-bold text-gray-800 text-4xl md:text-6xl lg:text-7xl xl:text-8xl">Desenvolvedor de Software</h1>
+    </div>
+  );
+};
+
+export default TitleHome;
