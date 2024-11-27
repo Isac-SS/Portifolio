08f4895a21758cea70415323a9ecea8e5eeee1c2
Isac-SS <isacsants679@gmail.com>
2024-11-27 15:28:07 -0300

    rafactor: restructure the title component

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -2,8 +2,8 @@
 const TitleHome = () => {
   return (
     <div className="flex justify-center flex-col gap-2">
-      <p  className="font-semibold text-[#088C37] text-4xl md:text-6xl lg:text-7xl xl:text-8xl">Olá, sou o Isac Santos,</p>
-      <h1 className="font-bold text-gray-800 text-4xl md:text-6xl lg:text-7xl xl:text-8xl">Desenvolvedor de Software</h1>
+      <p  className="font-semibold text-[#FAFAFAE6] text-4xl md:text-6xl lg:text-7xl xl:text-8xl">Desenvolvedor de Software</p>
+      <h1 className="font-bold text-[#f0340bc7] text-4xl md:text-6xl lg:text-7xl xl:text-8xl">Fullstack</h1>
     </div>
   );
 };
