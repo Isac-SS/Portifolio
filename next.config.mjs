b30e4ba5bdaf6ddf43909402c2ee515d4cfc239d
Isac-SS <isacsants679@gmail.com>
2025-04-24 15:18:30 -0300

    chore(config): permite carregamento de imagens do domínio do Hygraph no Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["us-west-2.graphassets.com"],
+        unoptimized: true,
+      },
+};
 
 export default nextConfig;
